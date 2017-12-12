0427a5e82ecf04de5c3b576bf677499cb19954a1
Won Maung Thein <wonmaungthein@gmail.com>
2017-12-12 12:35:49 -0800

    added JavaScript folders

---
+++ JavaScript intro/first.js
@@ -0,0 +1,31 @@
+alert ("hi. I am coming to you from first.js")
+
+
+var firstName = prompt("What is your first name?");
+// Now firstName should correspond to whatever the user typed!
+console.log("Hi, " + firstName + "!");
+console.log("How are you doing, " + firstName + "?");
+console.log("See you later, " + firstName + "!");
+
+
+// Create the following variables
+// name, which is a string set to your current name
+// dayOfBirth, which is a number set to the day of your birth month
+// See what happens when you have multiple variables of the same name.Which one takes precedence ?
+//     Write some JavaScript that prompt 's the user for their favorite color. 
+// Once the user has submitted a favorite color, 
+// log that color to the console along with a friendly message.
+// Create a string that contains both single quotes and double quotes.
+// What is the difference between null and undefined ?
+//     What is NaN in JavaScript ? What is the typeof NaN ?
+//     You can declare a variable by typing
+// var thing;.What is the value of thing ?
+
+var name = "Won Maung Thein";
+var dayOfBirth = 29;
+
+
+var favColor = prompt("what is your favourite color");
+console.log("My favourite colour is " + favColor + ".");
+var message = (' I love coding and my friend said that " I am so happy to code here. what\'s your favourite language?" ');
+
